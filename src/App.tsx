e53be1a98f95a671d2b39478ae57fb5c527a3aa0
Rusi Papazov <rusi.papazov@gmail.com>
2025-11-20 08:57:42 +0000

    add textarea in root app

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,40 @@
-import React from 'react'
+import React, { FormEvent, FormEventHandler, useState } from 'react'
 
-import './App.css'
 import ivionJson from "./definitions/ivion.json"
 import Game from './lib/dto/Game'
 import { gameFactory } from './lib/container'
 import { IDCodec } from "./lib/encoding/id-codec"
 
-
-function App() {
+const encodingTest = () => {
   const ids = [5,5,10,10,15,1000,1000]
   const encodedString = IDCodec.encodeToString(ids);
   const decoded = IDCodec.decodeFromString(encodedString);
 
-  let result: string
-  try {
-    const game: Game = gameFactory.build(ivionJson)
-    result =  `${JSON.stringify(game,  null, "  ")}`
-  } catch (e) {
-    const error: Error = e as Error
-    
-    result = error.message
+  //<pre>
+  // Encoded {ids.join(', ')} = {encodedString}
+  // <br></br>
+  // Decoded = {decoded.join(', ')}
+  // </pre>
+  return { ids, encodedString, decoded}
+}
+
+
+const App = () => {
+  const [input, setInput] = useState<string>('')
+  const [result, setResult] = useState<string>('')
+  // let result: string
+  // try {
+  //   const game: Game = gameFactory.build(ivionJson)
+  //   result =  `${JSON.stringify(game,  null, "  ")}`
+  // } catch (e) {
+  //   const error: Error = e as Error
+  const handleChange: FormEventHandler<HTMLTextAreaElement> = (event: FormEvent<HTMLTextAreaElement>) => {
+    setResult((event.target as HTMLTextAreaElement).value)
   }
   return (
     <>
-      <pre>{result}</pre>
-      <pre>
-        Encoded {ids.join(', ')} = {encodedString}
-        <br></br>
-        Decoded = {decoded.join(', ')}
-      </pre>
+      <textarea className="halfsies" onInput={handleChange}></textarea>
+      <pre className="halfsies">{result}</pre>
     </>
   );
 }
