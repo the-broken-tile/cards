824d755a216ef58c3c8ab7e6ee49d3f3f0c82d81
Rusi Papazov <rusi.papazov@gmail.com>
2025-11-21 19:31:13 +0200

    remove set dto

--- src/lib/dto/Card.ts
+++ src/lib/dto/Card.ts
@@ -1,11 +1,9 @@
 import Attribute from "./Attribute"
-import Set from "./Set"
 
 declare type Card = {
     readonly id: number
     readonly name: string
     readonly attributes: Attribute[]
-    readonly set?: Set
 }
 
 export default Card
